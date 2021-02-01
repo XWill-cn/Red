07643e8d72a9235514e78f572f0052382c87b0ba
XWill-cn <xuwei.cs.cn@gmail.com>
2021-02-01 17:57:48 +0800

    Initial Commit

---
+++ Red/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Red
+//
+//  Created by 徐维 on 2021/2/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
